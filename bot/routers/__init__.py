3a93a04e6704880c8ebc7ed6f4ab255599eb8e9a
shaxavipp <shaxaxbet77@gmail.com>
2026-08-22 17:08:27 +0500

    Add user info router to main router

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -11,6 +11,7 @@ from bot.routers.admin.stats import router as admin_stats_router
 from bot.routers.admin.votes_log import router as admin_votes_log_router
 from bot.routers.admin.withdrawals_admin import router as admin_withdrawals_router
 from bot.routers.user.account import router as user_account_router
+from bot.routers.user.info import router as user_info_router
 from bot.routers.user.my_votes import router as user_my_votes_router
 from bot.routers.user.my_withdrawals import router as user_my_withdrawals_router
 from bot.routers.user.referral import router as user_referral_router
@@ -40,3 +41,4 @@ root_router.include_router(user_my_withdrawals_router)
 root_router.include_router(user_withdrawal_router)
 root_router.include_router(user_referral_router)
 root_router.include_router(user_voting_router)
+root_router.include_router(user_info_router)
