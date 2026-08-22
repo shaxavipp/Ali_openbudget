8b56c53e892ecee1756e996769b9320eee24a699
shaxavipp <shaxaxbet77@gmail.com>
2026-08-22 16:21:11 +0500

    Update settings_states.py

--- bot/states/settings_states.py
+++ bot/states/settings_states.py
@@ -4,8 +4,4 @@ from aiogram.fsm.state import State, StatesGroup
 class GlobalSettingsStates(StatesGroup):
     waiting_vote_price = State()
     waiting_min_withdrawal = State()
-
-
-class ReferralSettingsStates(StatesGroup):
-    waiting_bonus = State()
-    waiting_promo_text = State()
+    waiting_donat_account = State()
